cb3c95c34a227aa087dae2dbffb52cbcdac0ae22
YASH BATANE <109904464+yashbatane@users.noreply.github.com>
2024-02-14 12:23:25 +0000

    discussion of variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,14 @@
 console.log("Yash Gurjar");
+
+
+const customerId = "1";
+var customerName = "Yash";
+let customerEmail = "Yash024@gmail.com"
+customerAge = 18;
+console.log(customerId); 
+
+console.table({customerId,customerEmail,customerName,customerAge});
+
+
+// Here we learned the differences between let , var, const and console.table({});
+// var is function scoped and let is blocked scope
