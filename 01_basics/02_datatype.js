d195a0a4c7440ea73548516dd31cc019c8bca05e
YASH BATANE <109904464+yashbatane@users.noreply.github.com>
2024-02-14 12:30:31 +0000

    Datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"  // treat all code as a newer version
+
+//  alert( 3 + 3 )  here we are using node js not browser so it is not working here and give the error
+
+
+// Primitive data type
+
+// number => 2 to power 53
+// bigint => for larger value
+// string => ""
+// boolean => true/false
+// null => standalone value  (type => object)
+// undefined => when we declared the variable but not intialized
+// symbol => unique
+
+
+// Non primitive data type
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
