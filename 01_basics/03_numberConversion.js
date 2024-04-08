7d7986f56f7f6ad0e7d384268b64d5e683b52d56
YASH BATANE <109904464+yashbatane@users.noreply.github.com>
2024-04-08 18:38:54 +0000

    numberConversion

---
+++ 01_basics/03_numberConversion.js
@@ -0,0 +1,29 @@
+let score=undefined;
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof score);
+
+// variableValues = Output
+
+// "33"=33;
+// "33abc"=Nan;
+// true=1; false=0;
+// null= 0;
+// undefined = Nan;
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
